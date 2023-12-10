9afd4571be81f01db0de5968f89187b2e5c3a24d
Burcu LACİN <burcu.lacin@demirorenteknoloji.com>
2023-12-10 22:46:02 +0300

    Create base redux

---
+++ src/redux/slices/fetchProductsSlice.ts
@@ -0,0 +1,30 @@
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const fetchProducts = createAsyncThunk(
+  'products/productsSlice',
+  async (API_URL: string) => {
+    const endpoint = `${API_URL}/products`;
+    try {
+      const { data } = await axios.get(endpoint);
+      return data;
+    } catch {
+      console.log(Error);
+    }
+  }
+);
+
+export const productsSlice = createSlice({
+  name: 'products',
+  initialState: {
+    products: [],
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchProducts.fulfilled, (state, action) => {
+      state.products = action.payload;
+    });
+  },
+});
+
+export default productsSlice.reducer;
