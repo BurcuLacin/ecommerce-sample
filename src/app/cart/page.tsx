9815c8c49caf55ad6d473a59b2a2324e76877b1d
Burcu LACİN <burcu.lacin@demirorenteknoloji.com>
2023-12-10 23:59:13 +0300

    some changes

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,6 +1,6 @@
-import React, { lazy } from 'react';
+import React, { lazy } from "react";
 
-const CartCom = lazy(() => import('@/components/cart/Cart'));
+const CartCom = lazy(() => import("@/components/cart/Cart"));
 
 const page = () => {
   return <CartCom />;
